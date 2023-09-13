074df6ebcf7b881e254303a03c13d32a4fd13fa5
aahuinink <a_huinink@hotmail.ca>
2023-09-12 18:05:16 -0700

    Completed lab 1

---
+++ labs/HelloWorld2023/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");  // some briliant commentary
+Console.WriteLine("Press any key to exit the application...");
+Console.ReadKey();
