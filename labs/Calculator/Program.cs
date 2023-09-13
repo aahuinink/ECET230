4af17bbe0d740ca94a9329ae68175b99e50d7b52
aahuinink <a_huinink@hotmail.ca>
2023-09-12 19:23:02 -0700

    added pretty colors

--- labs/Calculator/Program.cs
+++ labs/Calculator/Program.cs
@@ -1,17 +1,40 @@
-﻿// Declare variables and then initialize to zero.
+﻿
+
+// Declare variables and then initialize to zero.
+using System;
+
 double num1 = 0; double num2 = 0;
 
 // Display title as the C# console calculator app.
-Console.WriteLine("Console Calculator in C#\r");
-Console.WriteLine("------------------------\n");
+Console.WriteLine(@"
+    _________                            .__                          
+    \_   ___ \  ____   ____   __________ |  |   ____                  
+    /    \  \/ /  _ \ /    \ /  ___/  _ \|  | _/ __ \                 
+    \     \___(  <_> )   |  \\___ (  <_> )  |_\  ___/                 
+     \______  /\____/|___|  /____  >____/|____/\___  >                
+            \/            \/     \/                \/                 
+    _________        .__               .__          __                
+    \_   ___ \_____  |  |   ____  __ __|  | _____ _/  |_  ___________ 
+    /    \  \/\__  \ |  | _/ ___\|  |  \  | \__  \\   __\/  _ \_  __ \
+    \     \____/ __ \|  |_\  \___|  |  /  |__/ __ \|  | (  <_> )  | \/
+     \______  (____  /____/\___  >____/|____(____  /__|  \____/|__|   
+            \/     \/          \/                \/                  
+   
+
+By Aaron Huinink - C0520296
+            ");
 
 // Ask the user to type the first number.
 Console.WriteLine("Type a number, and then press Enter");
+Console.ForegroundColor = ConsoleColor.Green;
 num1 = Convert.ToDouble(Console.ReadLine());
+Console.ResetColor();
 
 // Ask the user to type the second number.
 Console.WriteLine("Type another number, and then press Enter");
+Console.ForegroundColor = ConsoleColor.Green;
 num2 = Convert.ToDouble(Console.ReadLine());
+Console.ResetColor();
 
 // Ask the user to choose an option.
 Console.WriteLine("Choose an option from the following list:");
@@ -25,13 +48,22 @@ Console.Write("Your option? ");
 switch (Console.ReadLine())
 {
     case "a":
-        Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
+        Console.Write($"Your result: {num1} + {num2} = ");
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(num1 + num2);
+        Console.ResetColor();
         break;
     case "s":
-        Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
+        Console.Write($"Your result: {num1} - {num2} = ");
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(num1 - num2);
+        Console.ResetColor();
         break;
     case "m":
-        Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
+        Console.Write($"Your result: {num1} * {num2} = ");
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(num1 * num2);
+        Console.ResetColor();
         break;
     case "d":
         while (num2 == 0)
@@ -39,7 +71,10 @@ switch (Console.ReadLine())
             Console.WriteLine("Please enter a non-zero divisor!");
             num2 = Convert.ToDouble(Console.ReadLine());
         }
-        Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
+        Console.Write($"Your result: {num1} / {num2} = ");
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(num1 / num2);
+        Console.ResetColor();
         break;
 }
 // Wait for the user to respond before closing.
