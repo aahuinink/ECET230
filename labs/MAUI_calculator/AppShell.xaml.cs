cbe785c7231a585f572d41a7e87d967b45cc3eb8
AaronH_MSI <a_huinink@hotmail.ca>
2023-09-17 17:07:05 -0700

    started building mainpage xaml ui for calculator

---
+++ labs/MAUI_calculator/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MAUI_calculator;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ labs/MAUI_calculator/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MAUI_calculator;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
