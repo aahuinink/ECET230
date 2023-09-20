7906e448ef98789a0d7ff48b704a21a7d8b60126
AaronH_MSI <a_huinink@hotmail.ca>
2023-09-20 13:47:08 -0700

    added clicked event handlers to all buttons

--- labs/MAUI_calculator/MainPage.xaml.cs
+++ labs/MAUI_calculator/MainPage.xaml.cs
@@ -7,5 +7,95 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void buttonOne_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void buttonTwo_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void buttonClear_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void button2nd_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void buttonThree_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void buttonPlus_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void buttonFour_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void buttonFive_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void buttonSix_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void buttonMinus_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void buttonSeven_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void buttonEight_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void buttonNine_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void buttonTimes_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void buttonDecimal_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void buttonZero_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void buttonEquals_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void buttonDivide_Clicked(object sender, EventArgs e)
+    {
+
+    }
 }
 
