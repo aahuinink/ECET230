be35f45d116a6e0bcdde1acbb8e07510bd72283a
AaronH_MSI <a_huinink@hotmail.ca>
2023-09-27 15:48:02 -0700

    screwing around

---
+++ labs/MeadowWifi/MeadowWifi/MeadowApp.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation;
+using Meadow.Foundation.Leds;
+using Meadow.Peripherals.Displays;
+using Meadow.Gateways.Bluetooth;
+using Meadow.Hardware;
+using Meadow.Peripherals.Leds;
+using MeadowWifi;
+using YamlDotNet.Serialization;
+
+public class MeadowApp : App<F7FeatherV2>
+{
+    readonly string SSID = "8c3bb16c0d954fb8b37999e1f040b279";
+    readonly string PASSWORD = "b1cb00bd69424cba937a597fabb93052";
+    readonly string IS_BLE_PAIRED = "8ee2de4ced7c4f2c92a1f984507d87e3";
+    readonly string HAS_JOINED_WIFI = "98dea8431f7443a3bf24f78650672361";
+
+    IDefinition bleTreeDefinition;
+
+    ICharacteristic Ssid;
+    ICharacteristic Password;
+    ICharacteristic IsBlePaired;
+    ICharacteristic HasJoinedWifi;
+
+    string ssid;
+    string password;
+
+    IWiFiNetworkAdapter wifi;
+
+    RgbPwmLed onboardLed;
+
+    public override Task Initialize()
+    {
+        onboardLed = new RgbPwmLed(
+            redPwmPin: Device.Pins.OnboardLedRed,
+            greenPwmPin: Device.Pins.OnboardLedGreen,
+            bluePwmPin: Device.Pins.OnboardLedBlue);
+        onboardLed.StartPulse(Color.Red);
+
+        wifi = Device.NetworkAdapters.Primary<IWiFiNetworkAdapter>();
+        wifi.NetworkConnected += WifiNetworkConnected;
+        wifi.NetworkDisconnected += WifiNetworkDisconnected;
+
+        bleTreeDefinition = GetDefinition();
+        Device.BluetoothAdapter.StartBluetoothServer(bleTreeDefinition);
+
+        Ssid.ValueSet += (s, e) => { ssid = (string)e; };
+        Password.ValueSet += (s, e) => { password = (string)e; };
+        //IsBlePaired.ValueSet += (s, e) =>
+        //{
+        //    DisplayController.Instance.UpdateBluetoothStatus((bool)e
+        //        ? "Paired"
+        //        : "Not Paired");
+        //};
+        HasJoinedWifi.ValueSet += async (s, e) =>
+        {
+            onboardLed.StartPulse(Color.Yellow);
+
+            if ((bool)e)
+            {
+                //DisplayController.Instance.UpdateWifiStatus("Connecting");
+                await wifi.Connect(ssid, password, TimeSpan.FromSeconds(45));
+
+                if (wifi.IsConnected)
+                {
+                    ConfigFileManager.CreateConfigFiles(ssid, password);
+                }
+            }
+            else
+            {
+                //DisplayController.Instance.UpdateWifiStatus("Disconnecting");
+                await wifi.Disconnect(false);
+            }
+        };
+
+        //DisplayController.Instance.UpdateStatus();
+
+        onboardLed.StartPulse(Color.Green);
+
+        return base.Initialize();
+    }
+
+    private void WifiNetworkConnected(INetworkAdapter sender, NetworkConnectionEventArgs args)
+    {
+        HasJoinedWifi.SetValue(true);
+        //DisplayController.Instance.UpdateWifiStatus("Connected");
+        onboardLed.StartPulse(Color.Magenta);
+    }
+
+    private void WifiNetworkDisconnected(INetworkAdapter sender)
+    {
+        HasJoinedWifi.SetValue(false);
+
+        ConfigFileManager.DeleteConfigFiles();
+
+        //DisplayController.Instance.UpdateBluetoothStatus("Not Paired");
+        //DisplayController.Instance.UpdateWifiStatus("Disconnected");
+        onboardLed.StartPulse(Color.Cyan);
+
+        Device.PlatformOS.Reset();
+    }
+
+    Definition GetDefinition()
+    {
+        var service = new Service(
+            name: "MeadowWifiService",
+            uuid: 253,
+
+            Ssid = new CharacteristicString(
+                name: nameof(Ssid),
+                uuid: SSID,
+                permissions: CharacteristicPermission.Read | CharacteristicPermission.Write,
+                properties: CharacteristicProperty.Read | CharacteristicProperty.Write,
+                maxLength: 256),
+            Password = new CharacteristicString(
+                name: nameof(Password),
+                uuid: PASSWORD,
+                permissions: CharacteristicPermission.Read | CharacteristicPermission.Write,
+                properties: CharacteristicProperty.Read | CharacteristicProperty.Write,
+                maxLength: 256),
+            IsBlePaired = new CharacteristicBool(
+                name: nameof(IsBlePaired),
+                uuid: IS_BLE_PAIRED,
+                permissions: CharacteristicPermission.Read | CharacteristicPermission.Write,
+                properties: CharacteristicProperty.Read | CharacteristicProperty.Write),
+            HasJoinedWifi = new CharacteristicBool(
+                name: nameof(HasJoinedWifi),
+                uuid: HAS_JOINED_WIFI,
+                permissions: CharacteristicPermission.Read | CharacteristicPermission.Write,
+                properties: CharacteristicProperty.Read | CharacteristicProperty.Write)
+        );
+
+        return new Definition("MeadowWifi", service);
+    }
+
+
+}
---
+++ labs/MeadowWifi/MeadowWifi/MeadowWifi.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation;
+using Meadow.Foundation.Leds;
+using Meadow.Peripherals.Leds;
+using Meadow.Foundation.Displays;
+using Meadow.Peripherals.Displays;
+using YamlDotNet.Serialization;
+
+namespace MeadowWifi
+{
+    public class MeadowConfigFile
+    {
+        public Device Device { get; set; } = new Device();
+        public Coprocessor Coprocessor { get; set; } = new Coprocessor();
+        public Network Network { get; set; } = new Network();
+    }
+    public class Device
+    {
+        public string Name { get; set; } = "MeadowWifi";
+    }
+    public class Coprocessor
+    {
+        public bool AutomaticallyStartNetwork { get; set; } = true;
+        public bool AutomaticallyReconnect { get; set; } = true;
+        public int MaximumRetryCount { get; set; } = 7;
+    }
+    public class Network
+    {
+        public int GetNetworkTimeAtStartup { get; set; } = 1;
+
+        public int NtpRefreshPeriod { get; set; } = 600;
+
+        public string[] NtpServers { get; set; } =
+        {
+            "0.pool.ntp.org",
+            "1.pool.ntp.org",
+            "2.pool.ntp.org",
+            "3.pool.ntp.org",
+        };
+
+        public string[] DnsServers { get; set; } =
+        {
+            "1.1.1.1",
+            "8.8.8.8"
+        };
+    }
+
+    public class WifiConfigFile
+    {
+        public Credentials Credentials { get; set; }
+
+        public WifiConfigFile(string ssid, string password)
+        {
+            Credentials = new Credentials()
+            {
+                Ssid = ssid,
+                Password = password
+            };
+        }
+    }
+    public class Credentials
+    {
+        public string Ssid { get; set; }
+        public string Password { get; set; }
+    }
+
+    public static class ConfigFileManager
+    {
+        public static void CreateConfigFiles(string ssid, string password)
+        {
+            CreateMeadowConfigFile();
+            CreateWifiConfigFile(ssid, password);
+        }
+
+        public static void DeleteConfigFiles()
+        {
+            DeleteMeadowConfigFile();
+            DeleteWifiConfigFile();
+        }
+
+        private static void CreateMeadowConfigFile()
+        {
+            try
+            {
+                var configFile = new MeadowConfigFile();
+
+                var serializer = new SerializerBuilder().Build();
+                var yaml = serializer.Serialize(configFile);
+
+                using (var fs = File.CreateText(Path.Combine(MeadowOS.FileSystem.UserFileSystemRoot, "meadow.config.yaml")))
+                {
+                    fs.WriteLine(yaml);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private static void CreateWifiConfigFile(string ssid, string password)
+        {
+            try
+            {
+                var configFile = new WifiConfigFile(ssid, password);
+
+                var serializer = new SerializerBuilder().Build();
+                var yaml = serializer.Serialize(configFile);
+
+                using (var fs = File.CreateText(Path.Combine(MeadowOS.FileSystem.UserFileSystemRoot, "wifi.config.yaml")))
+                {
+                    fs.WriteLine(yaml);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private static void DeleteMeadowConfigFile()
+        {
+            try
+            {
+                File.Delete($"{MeadowOS.FileSystem.UserFileSystemRoot}meadow.config.yaml");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private static void DeleteWifiConfigFile()
+        {
+            try
+            {
+                File.Delete($"{MeadowOS.FileSystem.UserFileSystemRoot}wifi.config.yaml");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+
+    public class DisplayController
+    {
+        private static readonly Lazy<DisplayController> instance =
+            new Lazy<DisplayController>(() => new DisplayController());
+        public static DisplayController Instance => instance.Value;
+
+        MicroGraphics graphics;
+
+        public DisplayController()
+        {
+            Initialize();
+        }
+
+        void Initialize()
+        {
+            var display = new (
+                i2cBus: MeadowApp.Device.CreateI2cBus(),
+                displayType: Ssd130xBase.DisplayType.OLED128x64);
+
+            graphics = new MicroGraphics(display)
+            {
+                Stroke = 1,
+                CurrentFont = new Font8x12(),
+            };
+
+            UpdateStatus();
+        }
+
+        public void UpdateStatus()
+        {
+            graphics.Clear();
+            graphics.DrawText(3, 3, "Bluetooth:");
+            graphics.DrawText(7, 18, "Not Paired");
+            graphics.DrawText(3, 33, "WIFI:");
+            graphics.DrawText(7, 48, "Disconnected");
+            graphics.Show();
+        }
+
+        public void UpdateBluetoothStatus(string status)
+        {
+            graphics.DrawRectangle(3, 18, 122, 12, false, true);
+            graphics.DrawText(7, 18, status);
+            graphics.Show();
+        }
+
+        public void UpdateWifiStatus(string status)
+        {
+            graphics.DrawRectangle(3, 48, 122, 12, false, true);
+            graphics.DrawText(7, 48, status);
+            graphics.Show();
+        }
+    }
+
+}
