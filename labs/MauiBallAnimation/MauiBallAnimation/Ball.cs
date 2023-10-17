52817725ae7353f81906009b1d4d87e2d5affbc5
AaronH_MSI <a_huinink@hotmail.ca>
2023-10-16 22:36:44 -0700

    created ball class and methods

---
+++ labs/MauiBallAnimation/MauiBallAnimation/Ball.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiBallAnimation;
+
+public class Ball
+{
+    public double X;        // x coordinate of the ball
+    public double Y;        // y coordinate of the ball
+    public double Radius;   // ball radius
+    public double XVel;     // velocity in x direction
+    public double YVel;     // velocity in y direction
+    public byte R, G, B;    // ball colour values
+
+    // class constructor
+    public Ball(double x, double y, double radius, double xVel, double yVel, byte r, byte g, byte b)
+    {
+        X = x;
+        Y = y;
+        Radius = radius;
+        XVel = xVel;
+        YVel = yVel;
+        R = r;
+        G = g;
+        B = b;
+    }
+
+    public void Advance(double ballSpeed, double width, double height)
+    {
+        MoveBall(ballSpeed);
+        Bounce(width, height);
+    }
+
+    private void Bounce(double width, double height)
+    {
+        double minX = Radius;           // minimum x value of the ball
+        double minY = Radius;           // minimum y value of the ball
+        double maxX = width - Radius;   // maximum x value of the ball
+        double maxY = height - Radius;  // maximum y value of the ball
+
+        // when ball reaches the left wall
+        if(X < minX)
+        {
+            double over = minX - X;     // calculate position overage
+            X = minX + over;            // reset ball positin
+            XVel = -XVel;               // reverse XVel direction
+        }
+        // when ball reaches the bottom
+        if(Y < minY)
+        {
+            double over = minY - Y;     // calculate position overage
+            Y = minY + over;            // reset ball position
+            YVel= -YVel;                // reverse YVel direction
+        }
+
+        // do the same for the rest of the page limits
+        if (X > maxX)
+        {
+            double over = X - maxX;
+            X = maxX - over;
+            XVel = -XVel;
+        }
+        if (Y > maxY)
+        {
+            double over = Y - maxY;
+            Y = maxY - over;
+            YVel=-YVel;
+        }
+    }
+
+    private void MoveBall(double ballSpeed)
+    {
+        X += XVel * ballSpeed;
+        Y += YVel * ballSpeed;
+    }
+
+    public void Draw(ICanvas canvas)
+    {
+        canvas.FillColor = Color.FromRgb(R,G,B);
+        canvas.FillCircle((float)X, (float)Y, (float)Radius);
+    }
+}
+
--- labs/MauiBallAnimation/MauiBallAnimation/BallField.cs
+++ labs/MauiBallAnimation/MauiBallAnimation/BallField.cs
@@ -4,13 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MauiBallAnimation
+namespace MauiBallAnimation;
+
+public class BallField :IDrawable //
 {
-    public class BallField :IDrawable //
+    Ball ball = new Ball(100, 100, 20, 10, 11, 128, 128, 128);
+    public BallField()
     {
-        public void Draw(ICanvas canvas, RectF dirtyRect)
-        {
-            
-        }
+        
+    }
+
+    public void Draw(ICanvas canvas, RectF dirtyRect)
+    {
+        ball.Advance(25, 1000, 1000);
+        ball.Draw(canvas);
     }
 }
+
