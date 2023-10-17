fe47577f606d6c6ad108686dc7c446b7ca815382
AaronH_MSI <a_huinink@hotmail.ca>
2023-10-16 22:13:56 -0700

    created timer and elapsed event handler

--- labs/MauiBallAnimation/MauiBallAnimation/MainPage.xaml.cs
+++ labs/MauiBallAnimation/MauiBallAnimation/MainPage.xaml.cs
@@ -1,7 +1,12 @@
-﻿namespace MauiBallAnimation;
+﻿using System.Timers;
+using Timer = System.Timers.Timer;
+
+namespace MauiBallAnimation;
 
 public partial class MainPage : ContentPage
 {
+	private double frameDuration = 30;      // the frame duration in milliseconds
+	public Timer timer;						// the timer
 
 	public MainPage()
 	{
@@ -9,15 +14,19 @@ public partial class MainPage : ContentPage
 	}
 	private void ContentPage_Loaded(object sender, EventArgs e)
     {
-		DrawBall();
+		timer = new Timer();			// create local instance of timer
+		timer.Interval = frameDuration; // set timer interval to the frame duration
+		timer.Elapsed += new ElapsedEventHandler(updateBallField);	// create event handler
+		timer.Start();		// start the timer
     }
 
-	private void DrawBall()
-	{
-		var graphicsView = this.BallGraphicsView;
-		//var ballFieldDrawable = (BallField)graphicsView.Drawable;
-		//ballFieldDrawable;
-		graphicsView.Invalidate();
-	}
+    private void updateBallField(object sender, ElapsedEventArgs e)
+    {
+        var graphicsView = this.BallGraphicsView;		// create a graphicsView instance fromt the xaml
+        //var ballFieldDrawable = (BallField)graphicsView.Drawable;
+        //ballFieldDrawable;
+        graphicsView.Invalidate();	// invalidate to update
+    }
+
 }
 
