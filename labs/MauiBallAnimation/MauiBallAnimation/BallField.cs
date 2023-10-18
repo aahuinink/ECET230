d2e0e7a855b86d4bb95efc514166d6c3fe81a181
AaronH_MSI <a_huinink@hotmail.ca>
2023-10-18 15:33:02 -0700

    added entries, event handlers, ballcount property

--- labs/MauiBallAnimation/MauiBallAnimation/BallField.cs
+++ labs/MauiBallAnimation/MauiBallAnimation/BallField.cs
@@ -9,11 +9,20 @@ namespace MauiBallAnimation;
 public class BallField :IDrawable //
 {
     public Ball[] Balls;    // array of balls
+    
+    public int BallCount
+    {
+        get { return ballCount; }
+        set { ballCount = value;
+            newBalls(ballCount);
+        }
+    }
+    private int ballCount { get; set; } = 2;
+    public int ballSpeed { get; set; } = 5;
 
-    public int ballCount { get; set; } = 2;
-    public int ballSpeed { get; set; } = 100;
-    public double width { get; set; } = 1000;
-    public double height { get; set; } = 1000;
+    public double width;
+    public double height;
+
 
     public BallField()
     {
@@ -29,8 +38,8 @@ public class BallField :IDrawable //
             double X = rand.NextDouble() * width;
             double Y = rand.NextDouble() * height;
             double Radius = rand.NextDouble() * 5 + 5;
-            double XVel = rand.NextDouble() - .5;
-            double YVel = rand.NextDouble() - .5;
+            double XVel = (rand.NextDouble() - .5) * 10;
+            double YVel = (rand.NextDouble() - .5) * 10;
             byte R = (byte)(rand.Next(50, 255));
             byte G = (byte)(rand.Next(50, 255));
             byte B = (byte)(rand.Next(50, 255));
@@ -50,12 +59,12 @@ public class BallField :IDrawable //
 
     public void Draw(ICanvas canvas, RectF dirtyRect)
     {
+
         foreach (Ball ball in Balls)
         {
             ball.Advance(ballSpeed, width, height);
             ball.Draw(canvas);
         }
-        
     }
 }
 
--- labs/MauiBallAnimation/MauiBallAnimation/MainPage.xaml.cs
+++ labs/MauiBallAnimation/MauiBallAnimation/MainPage.xaml.cs
@@ -14,7 +14,7 @@ public partial class MainPage : ContentPage
 	}
 	private void ContentPage_Loaded(object sender, EventArgs e)
     {
-		timer = new Timer();			// create local instance of timer
+        timer = new Timer();			// create local instance of timer
 		timer.Interval = frameDuration; // set timer interval to the frame duration
 		timer.Elapsed += new ElapsedEventHandler(updateBallField);	// create event handler
 		timer.Start();      // start the timer
@@ -22,11 +22,69 @@ public partial class MainPage : ContentPage
 
     private void updateBallField(object sender, ElapsedEventArgs e)
     {
-        var graphicsView = this.BallGraphicsView;		// create a graphicsView instance fromt the xaml
-        var ballFieldDrawable = (BallField)graphicsView.Drawable;
-		//ballFieldDrawable;
+		var graphicsView = this.BallGraphicsView;
+
+		var ballView = (BallField)graphicsView.Drawable;
+		ballView.width = graphicsView.Width;
+		ballView.height = graphicsView.Height;
+		ballView.height = graphicsView.Height;
+		
         graphicsView.Invalidate();	// invalidate to update
     }
 
+    private void ContentPage_SizeChanged(object sender, EventArgs e)
+    {
+		var graphicsView = this.BallGraphicsView;
+		var ballFieldDrawable = (BallField)graphicsView.Drawable;
+
+		if (Width <= 0 || Height <= 0)
+		{
+			return;
+		}
+		graphicsView.WidthRequest = Width;
+		graphicsView.HeightRequest = Height;
+		ballFieldDrawable.width = Width;
+		ballFieldDrawable.height = Height;
+    }
+
+    private void BallSpeed_Entry_TextChanged(object sender, TextChangedEventArgs e)
+    {
+		var ballFieldDrawable = (BallField)this.BallGraphicsView.Drawable;		// create drawable
+		int speed;																// create int to hold speed value
+		if(int.TryParse(e.NewTextValue, out speed))								// if valid input
+		{
+            BallSpeed_Entry.Text = e.NewTextValue;								// change the text
+            ballFieldDrawable.ballSpeed = speed;								// set the new speed
+			return;
+		}	
+		BallSpeed_Entry.Text = e.NewTextValue;									// otherwise just change the text
+    }
+
+
+    private void BallCount_Entry_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        var ballFieldDrawable = (BallField)this.BallGraphicsView.Drawable;	// create drawable
+		int count;															// create int to hold count value from entry
+		if( int.TryParse(e.NewTextValue,out count))							// if valid
+		{
+			BallCount_Entry.Text = e.NewTextValue;							// change the text
+			ballFieldDrawable.BallCount = count;                            // set the new ball count
+			
+			return;
+		}
+		ballFieldDrawable.BallCount = count; return;						// otherwise just change the text
+    }
+
+    private void FrameRate_Entry_TextChanged(object sender, TextChangedEventArgs e)
+    {
+		if(double.TryParse(e.NewTextValue, out frameDuration))      // if input is valid
+		{
+			timer.Interval = (frameDuration > 16) ? frameDuration : 16;         // set the timer interval to the new frame duration, minimum allowable is 16ms
+			FrameRate_Entry.Text = frameDuration.ToString();                    // set the text to the frame duration	
+			return;	
+        }
+
+		FrameRate_Entry.Text = frameDuration.ToString();			// otherwise just set to the current frame duration 
+    }
 }
 
