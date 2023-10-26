f152a3ddd0e6f798551fd48010a4486ae77eb25e
AaronH_MSI <a_huinink@hotmail.ca>
2023-10-25 18:40:57 -0700

    closing up shop

--- labs/MauiUSBmeadow20231024/MauiUSBmeadow20231024/MainPage.xaml.cs
+++ labs/MauiUSBmeadow20231024/MauiUSBmeadow20231024/MainPage.xaml.cs
@@ -1,19 +1,56 @@
-﻿namespace MauiUSBmeadow20231024;
+﻿using System.IO.Ports;
+
+namespace MauiUSBmeadow20231024;
 
 public partial class MainPage : ContentPage
 {
+    private bool bPortOpen=false;
+    private string newPacket = "";
 
+    SerialPort serialPort = new SerialPort();
 	public MainPage()
 	{
 		InitializeComponent();
+        string[] ports = SerialPort.GetPortNames();
+        pkrComPort.ItemsSource = ports;
+        pkrComPort.SelectedIndex = ports.Length;
+        Loaded += MainPage_Loaded;
 	}
 
+    private void MainPage_Loaded(object sender, EventArgs e)
+    {
+        serialPort.BaudRate = 115200;
+        serialPort.ReceivedBytesThreshold = 1;
+        serialPort.DataReceived += SerialPort_DataReceived;
+    }
+
+    private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+    {
+        newPacket = serialPort.ReadLine();
+    }
+
     private void btnOpenClose_Clicked(object sender, EventArgs e)
+    {
+        if (!bPortOpen)
+        {
+            serialPort.PortName = pkrComPort.SelectedItem.ToString();
+            serialPort.Open();
+            btnOpenClose.Text = "Close";
+            bPortOpen = true;
+            return;
+        }
+        serialPort.Close();
+        btnOpenClose.Text = "Open";
+        bPortOpen= false;
+        return;
+    }
+
+    private void btnClear_Clicked(object sender, EventArgs e)
     {
 
     }
 
-    private void btnClear_Clicked(object sender, EventArgs e)
+    private void btnSend_Clicked(object sender, EventArgs e)
     {
 
     }
