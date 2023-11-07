92232d2713bb75fc7d1d12eaa6191c7d56de2f80
AaronH_MSI <a_huinink@hotmail.ca>
2023-11-06 17:20:52 -0800

    created solar class with fields and encapsulations

---
+++ labs/MauiUSBmeadow20231024/MauiUSBmeadow20231024/Solar.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiSolar
+
+{
+    internal class Solar
+    {
+        private int[] _analogValues;
+        private float _panelVoltage;
+        private float _panelCurrent;
+        private float _batteryVoltage;
+        private float _batteryCurrent;
+        private string _batteryStatus;
+        private float _redLEDCurrent;
+        private float _greenLEDCurrent;
+
+        public Solar() { }
+
+        // ----- Encapsulations ----- //
+        /// <summary>
+        /// The panel voltage
+        /// </summary>
+        public float PanelVoltage { get => _panelVoltage; set => _panelVoltage = value; }
+        /// <summary>
+        /// the panel current
+        /// </summary>
+        public float PanelCurrent { get => _panelCurrent; set => _panelCurrent = value; }
+        /// <summary>
+        /// The battery/super cap voltage.
+        /// </summary>
+        public float BatteryVoltage { get => _batteryVoltage; set => _batteryVoltage = value; }
+        /// <summary>
+        /// The battery/super cap current. Positive indicates charging.
+        /// </summary>
+        public float BatteryCurrent { get => _batteryCurrent; set => _batteryCurrent = value; }
+        /// <summary>
+        /// The charging status of the battery.
+        /// </summary>
+        public string BatteryStatus { get => _batteryStatus; set => _batteryStatus = value; }
+        /// <summary>
+        /// The current through the red LED.
+        /// </summary>
+        public float RedLEDCurrent { get => _redLEDCurrent; set => _redLEDCurrent = value; }
+        /// <summary>
+        /// The current through the green LED.
+        /// </summary>
+        public float GreenLEDCurrent { get => _greenLEDCurrent; set => _greenLEDCurrent = value; }
+        /// <summary>
+        /// Analog values from the meadow board.
+        /// </summary>
+        public int[] AnalogValues { get => _analogValues; set => _analogValues = value; }
+    }
+}
